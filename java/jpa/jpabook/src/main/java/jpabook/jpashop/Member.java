b5400da48b62e5fbd42d4319dc9168190cdfaf8a
DongMin <dongminkim5067@gmail.com>
2024-10-09 21:51:02 +0900

    add : spring JPA setting and JPA test

--- java/jpa/jpabook/src/main/java/jpabook/jpashop/Member.java
+++ java/jpa/jpabook/src/main/java/jpabook/jpashop/Member.java
@@ -2,20 +2,21 @@ package jpabook.jpashop;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 /**
- * @author ubd2000
+ * @author DongMin Kim
  */
 @Entity
 @Getter
 @Setter
 public class Member {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    private String userName;
+    private String username;
 
 }
