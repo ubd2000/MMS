b35f439d62c329bfcde91397bd54ab4193a631b9
DongMin <dongminkim5067@gmail.com>
2025-02-28 16:05:01 +0900

    add : spring data JPA study를 위한 프로젝트 추가

---
+++ java/jpa/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -0,0 +1,26 @@
+package study.datajpa.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Member;
+
+/**
+ * @author DongMin Kim
+ */
+
+@Repository
+public class MemberJpaRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
---
+++ java/jpa/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,35 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author DongMin Kim
+ */
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        assertEquals(findMember.getId(), member.getId());
+        assertEquals(findMember.getUsername(), member.getUsername());
+        assertEquals(findMember, member);
+    }
+
+}
